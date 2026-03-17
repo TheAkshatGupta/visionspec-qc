0b0ccb6643f42c25919edabe0517d0d616f7d76a
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-17 20:08:06 +0530

    Added CNN model

--- src/models/cnn_model.py
+++ src/models/cnn_model.py
@@ -1,1 +1,36 @@
- 
+import tensorflow as tf
+from tensorflow.keras import layers, models
+
+def build_cnn_model(input_shape=(224, 224, 3)):
+    model = models.Sequential()
+
+    # Conv Block 1
+    model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=input_shape))
+    model.add(layers.MaxPooling2D(2,2))
+
+    # Conv Block 2
+    model.add(layers.Conv2D(64, (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D(2,2))
+
+    # Conv Block 3
+    model.add(layers.Conv2D(128, (3,3), activation='relu'))
+    model.add(layers.MaxPooling2D(2,2))
+
+    # Flatten
+    model.add(layers.Flatten())
+
+    # Dense Layers
+    model.add(layers.Dense(128, activation='relu'))
+    model.add(layers.Dropout(0.5))
+
+    # Output Layer
+    model.add(layers.Dense(1, activation='sigmoid'))
+
+    # Compile
+    model.compile(
+        optimizer='adam',
+        loss='binary_crossentropy',
+        metrics=['accuracy']
+    )
+
+    return model
