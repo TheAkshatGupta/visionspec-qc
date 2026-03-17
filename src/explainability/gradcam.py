c2c1933ab1cf2ca39fac3a9faf5422d5551fe271
TheAkshatGupta <2k23.csds2310601@gmail.com>
2026-03-17 20:12:03 +0530

    Added Grad-CAM implementation

--- src/explainability/gradcam.py
+++ src/explainability/gradcam.py
@@ -1,1 +1,35 @@
- 
+import numpy as np
+import tensorflow as tf
+import cv2
+
+def get_gradcam_heatmap(model, image, last_conv_layer_name):
+    grad_model = tf.keras.models.Model(
+        [model.inputs],
+        [model.get_layer(last_conv_layer_name).output, model.output]
+    )
+
+    with tf.GradientTape() as tape:
+        conv_outputs, predictions = grad_model(image)
+        loss = predictions[:, 0]
+
+    grads = tape.gradient(loss, conv_outputs)
+
+    pooled_grads = tf.reduce_mean(grads, axis=(0, 1, 2))
+    conv_outputs = conv_outputs[0]
+
+    heatmap = conv_outputs @ pooled_grads[..., tf.newaxis]
+    heatmap = tf.squeeze(heatmap)
+
+    heatmap = tf.maximum(heatmap, 0) / tf.math.reduce_max(heatmap)
+    return heatmap.numpy()
+
+
+def overlay_heatmap(heatmap, image_path, alpha=0.4):
+    img = cv2.imread(image_path)
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    superimposed_img = cv2.addWeighted(img, 1 - alpha, heatmap, alpha, 0)
+
+    return superimposed_img
